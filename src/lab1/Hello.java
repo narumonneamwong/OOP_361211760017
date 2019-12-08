334f5605f3d16a0592ae50bb211815e1dc980807
Dawny <dawnysee@gmail.com>
2019-12-08 11:11:11 +0700

    Add massage

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello Word,I am Dawny");
+        System.out.println("MIT");
+        System.out.println("Faculty of Managmant Technology");
 
 
 
